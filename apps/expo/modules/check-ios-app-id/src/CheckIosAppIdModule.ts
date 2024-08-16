1bf0cc541d5932551d5f56de00949e1a1f5e6cd6
Adrian Castro <22133246+castdrian@users.noreply.github.com>
2024-08-16 11:39:32 +0200

    fix: eat my ass

--- apps/expo/modules/check-ios-app-id/src/CheckIosAppIdModule.ts
+++ apps/expo/modules/check-ios-app-id/src/CheckIosAppIdModule.ts
@@ -7,4 +7,5 @@ interface CheckIosAppIdModule {
 
 // It loads the native module object from the JSI or falls back to
 // the bridge module (from NativeModulesProxy) if the remote debugger is on.
+// eslint-disable-next-line @typescript-eslint/no-unnecessary-type-assertion
 export default requireNativeModule("CheckIosAppId") as CheckIosAppIdModule;
--- apps/expo/modules/check-ios-marketplace/src/CheckIosMarketplaceModule.ts
+++ apps/expo/modules/check-ios-marketplace/src/CheckIosMarketplaceModule.ts
@@ -17,6 +17,7 @@ interface CheckIosMarketplaceModule {
 
 // It loads the native module object from the JSI or falls back to
 // the bridge module (from NativeModulesProxy) if the remote debugger is on.
+// eslint-disable-next-line @typescript-eslint/no-unnecessary-type-assertion
 export default requireNativeModule(
   "CheckIosMarketplace",
 ) as CheckIosMarketplaceModule;
